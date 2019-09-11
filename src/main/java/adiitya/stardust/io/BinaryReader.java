6ef7c6da8d9ed1f1df56f1e3516a55183efe4a46
AdityaSF <A44itya@hotmail.com>
2019-09-10 20:44:29 -0600

    Add javadoc for BinaryReader

--- src/main/java/adiitya/stardust/io/BinaryReader.java
+++ src/main/java/adiitya/stardust/io/BinaryReader.java
@@ -14,35 +14,62 @@ public class BinaryReader {
 	private final ByteBuffer buf;
 
 	public BinaryReader(InputStream input) throws IOException {
-
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		int len;
-		byte[] bytes = new byte[1024];
-
-		while ((len = input.read(bytes)) != -1)
-			out.write(bytes, 0, len);
-
-		buf = ByteBuffer.wrap(out.toByteArray()).order(ByteOrder.LITTLE_ENDIAN);
-		out.close();
-		input.close();
+		this.buf = readBytes(input);
 	}
 
+	/**
+	 * This method reads a byte from the buffer using the default
+	 * method from ByteBuffer.
+	 *
+	 * @return the byte
+	 * @see ByteBuffer#get()
+	 */
 	public byte readByte() {
 		return buf.get();
 	}
 
+	/**
+	 * This method reads a signed integer from the buffer using the default
+	 * method from ByteBuffer.
+	 *
+	 * @return the integer
+	 * @see ByteBuffer#getInt()
+	 */
 	public int readInt() {
 		return buf.getInt();
 	}
 
+	/**
+	 * This methods reads an unsigned integer from the buffer. It uses
+	 * {@link Integer#toUnsignedLong(int)} as opposed to manually converting
+	 * the integer. It is then casted back to an integer and returned.
+	 *
+	 * @return the integer
+	 * @see Integer#toUnsignedLong(int)
+	 * @see #readInt()
+	 */
 	public int readUnsignedInt() {
 		return (int) Integer.toUnsignedLong(readInt());
 	}
 
+	/**
+	 * This method reads a signed long from the buffer using the default
+	 * method from ByteBuffer.
+	 *
+	 * @return the long
+	 * @see ByteBuffer#getLong()
+	 */
 	public long readLong() {
 		return buf.getLong();
 	}
 
+	/**
+	 * Reads a String of the specified length. This string uses UTF-8
+	 * encoding.
+	 *
+	 * @param len the length of the String
+	 * @return the String
+	 */
 	public String readString(int len) {
 
 		byte[] bytes = new byte[len];
@@ -51,10 +78,21 @@ public class BinaryReader {
 		return new String(bytes);
 	}
 
+	/**
+	 * Reads a String where the first byte is the amount of characters.
+	 * @return the String
+	 * @see #readString(int)
+	 */
 	public String readString() {
 		return readString(readByte());
 	}
 
+	/**
+	 * Reads a boolean that is the length specified.
+	 *
+	 * @param len the byte length of the boolean
+	 * @return false if the number read is 0, otherwise true
+	 */
 	public boolean readBoolean(int len) {
 
 		byte[] bytes = new byte[len];
@@ -63,13 +101,52 @@ public class BinaryReader {
 		return !new BigInteger(bytes).equals(BigInteger.ZERO);
 	}
 
+	/**
+	 * Reads a boolean that is 1 byte long.
+	 *
+	 * @return the boolean
+	 * @see #readBoolean(int)
+	 */
 	public boolean readBoolean() {
 		return readBoolean(1);
 	}
 
+	/**
+	 * This method skips the specified amount of bytes by reading
+	 * them into a byte array.
+	 *
+	 * @param len the amoount of bytes to skip
+	 */
 	public void skip(int len) {
 
 		byte[] bytes = new byte[len];
 		buf.get(bytes);
+	}
+
+	/**
+	 * Reads an InputStream into a ByteBuffer. This method reads the
+	 * InputStream into a ByteArrayOutputStream before wrapping
+	 * the resulting byte array in a ByteBuffer. The ByteBuffer with
+	 * be little endian.
+	 *
+	 * @param input the InputStream to be read
+	 * @return the buffer
+	 * @throws IOException if an error occurs while reading from the InputStream
+	 */
+	static ByteBuffer readBytes(InputStream input) throws IOException {
+
+		try (ByteArrayOutputStream byteStream = new ByteArrayOutputStream()) {
+
+			int bytesRead;
+			byte[] data = new byte[1024];
+
+			while ((bytesRead = input.read(data)) != -1)
+				byteStream.write(data, 0, bytesRead);
+
+			return ByteBuffer.wrap(byteStream.toByteArray())
+					.order(ByteOrder.LITTLE_ENDIAN);
+		} finally {
+			input.close();
+		}
 	}
 }
